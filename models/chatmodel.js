f6a3d865f0b7bbf90394f619913a5ef853931ad0
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2025-04-23 18:33:08 +0530

    Delete models directory

--- models/chatmodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ChatSchema = new mongoose.Schema({
-  username: String,
-  text: String,
-  timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Chat", ChatSchema);
